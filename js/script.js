a04b56c228d3d9530d2fbccb20c6ebc339d358ff
GiordanoDiego <diegomgiordano96@gmail.com>
2023-12-05 10:25:13 +0100

    upgrade class

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+console.log('nomeVariabile', nomeVariabile, typeof nomeVariabile);
+
+document.getElementById('idHtml').innerHTML = noveVariabileJS;
+
+//valore input 
+    userName = document.getElementById('userName');
+    userNameValue = userName.value;
+
+//controllare se il valore è Nan
+    if(isNaN(kmValue))
+
+*/
+
