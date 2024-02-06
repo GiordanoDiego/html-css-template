d3f83f634c0b007af8a8afc821b1f4907861f872
GiordanoDiego <diegomgiordano96@gmail.com>
2024-02-06 15:03:12 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- FONT-FAMILY -->
+
+        <!-- LINK CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css">
+        <!-- TITLE PAGE -->
+        <title>Document</title>
+    </head>
+
+        <!-- START BODY -->
+    <body class="debug">
+        <!-- START MAIN HEADER -->
+        <header>
+            <div class="container">
+                HEAD
+            </div>
+        </header>
+        <!-- END MAIN HEADER -->
+
+        <!-- START MAIN -->
+        <main>
+            <div class="container">
+                MAIN
+            </div>
+        </main>
+        <!-- END MAIN -->
+
+        <!-- START FOOTER -->
+        <footer>
+            <div class="container">
+                FOOTER
+            </div>
+        </footer>
+        <!-- END FOOTER -->
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
